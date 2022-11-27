434079d4cccb7139c1470a3d032f2c774c7cdc2a
Fazekas László <fazekas@inf.u-szeged.hu>
2022-11-27 15:33:53 +0100

    minor refactor to be able to send reservation

--- src/app/foglalas/foglalas.component.ts
+++ src/app/foglalas/foglalas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {FormBuilder, Validators} from '@angular/forms';
+import { FoglalasModel, FoglalasReszletModel } from '../models/foglalas.model';
 
 
 @Component({
@@ -9,6 +10,24 @@ import {FormBuilder, Validators} from '@angular/forms';
   styleUrls: ['./foglalas.component.css']
 })
 export class FoglalasComponent implements OnInit {
+  public first_data: FoglalasModel = {
+    "szemelyi": "",
+    "vezeteknev": "",
+    "keresztnev": "",
+    "nem": "",
+    "szul_datun": "",
+    "telepules": "",
+    "utca": "",
+    "hazszam": "",
+    "email": "",
+  }
+
+  public second_data: FoglalasReszletModel = {
+    "erkezes": "",
+    "foszam": 0,
+    "szobatipus": "",
+    "tavozas": ""
+  }
 
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
@@ -19,6 +38,8 @@ export class FoglalasComponent implements OnInit {
   isEditable = false;
 
   constructor(private _formBuilder: FormBuilder, private http:HttpClient) { }
+
+
   onSubmit(data: any) {
     console.warn(data);
     this.http.post('http://127.0.0.1:8000/', data).subscribe((result) => {
